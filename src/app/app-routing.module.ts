4f48ae9ba78a5cfd0b45a2be45cc9f9f684b3302
Derek Watson <dwatson@mail.mccneb.edu>
2020-04-13 21:13:43 -0500

    Add route to take-quiz component with id parameter for quiz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { QuizComponent } from "./quiz/quiz.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { TakeQuizComponent } from './take-quiz/take-quiz.component';
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
-  { path: "quiz/:id", component: QuizComponent }
+  { path: "quiz/:id", component: QuizComponent },
+  { path: "take-quiz/:id", component: TakeQuizComponent }
 ];
 
 @NgModule({
