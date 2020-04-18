510a7392d78115aaa78129e0e7242596736abf1f
Derek Watson <dwatson@mail.mccneb.edu>
2020-04-18 10:13:18 -0500

    Add some constructors

--- src/app/question-submission/shared/answerSubmission.model.ts
+++ src/app/question-submission/shared/answerSubmission.model.ts
@@ -1,4 +1,5 @@
 import { QuestionSubmission } from "./questionSubmission.model";
+import { Answer } from "src/app/question/shared/answer.model";
 
 export class AnswerSubmission {
   id: number;
@@ -7,6 +8,28 @@ export class AnswerSubmission {
   isSelected: boolean;
   question?: QuestionSubmission;
   question_id?: number;
+
+  constructor(
+    id: number,
+    content: string,
+    isCorrect: boolean,
+    isSelected: boolean
+  ) {
+    this.id = id;
+    this.content = content;
+    this.isCorrect = isCorrect;
+    this.isSelected = isSelected;
+  }
+
+  static convertAnswerToAnswerSubmission(answer: Answer): AnswerSubmission {
+    let a: AnswerSubmission = new AnswerSubmission(
+      answer.id,
+      answer.content,
+      answer.isCorrect,
+      false
+    );
+    return a;
+  }
 }
 
 export class AnswerSubmissionApiResponse {
--- src/app/question-submission/shared/questionSubmission.model.ts
+++ src/app/question-submission/shared/questionSubmission.model.ts
@@ -1,9 +1,15 @@
-import { AnswerSubmission } from './answerSubmission.model';
-import { QuizSubmission } from '../../quiz-submission/shared/quizSubmission.model';
+import { AnswerSubmission } from "./answerSubmission.model";
+import { QuizSubmission } from "../../quiz-submission/shared/quizSubmission.model";
 
 export class QuestionSubmission {
   id: number;
   content: string;
   quizzes?: QuizSubmission[];
   answers: AnswerSubmission[];
+
+  constructor(id: number, content: string, answers: AnswerSubmission[]) {
+    this.id = id;
+    this.content = content;
+    this.answers = answers;
+  }
 }
